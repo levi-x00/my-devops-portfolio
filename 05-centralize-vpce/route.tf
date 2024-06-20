a4df9f5e6acd88ad0d86aba6e58d27f185e19ca2
levi-x00 <m.r.pahlevi00@gmail.com>
2024-06-20 10:27:36 +0700

    update diagram centralize vpce

--- 05-centralize-vpce/route.tf
+++ 05-centralize-vpce/route.tf
@@ -1,12 +1,25 @@
 # ============ spoke 1 <-> vpce VPC ================
 resource "aws_route" "spoke1_vpce" {
-  route_table_id            = module.vpc_spoke1.private_rt_id
-  destination_cidr_block    = var.vpc_spoke1_cidr
-  vpc_peering_connection_id = "pcx-45ff3dc1"
+  route_table_id         = module.vpc_spoke1.private_rt_id
+  destination_cidr_block = var.vpc_vpce_cidr
+  transit_gateway_id     = aws_ec2_transit_gateway.mytgw.id
 }
 
 resource "aws_route" "vpce_spoke1" {
-  route_table_id            = aws_route_table.testing.id
-  destination_cidr_block    = var.vpc_vpce_cidr
-  vpc_peering_connection_id = "pcx-45ff3dc1"
+  route_table_id         = module.vpce.private_rt_id
+  destination_cidr_block = var.vpc_spoke1_cidr
+  transit_gateway_id     = aws_ec2_transit_gateway.mytgw.id
+}
+
+# ============ spoke 2 <-> vpce VPC ================
+resource "aws_route" "spoke2_vpce" {
+  route_table_id         = module.vpc_spoke2.private_rt_id
+  destination_cidr_block = var.vpc_vpce_cidr
+  transit_gateway_id     = aws_ec2_transit_gateway.mytgw.id
+}
+
+resource "aws_route" "vpce_spoke2" {
+  route_table_id         = module.vpce.private_rt_id
+  destination_cidr_block = var.vpc_spoke2_cidr
+  transit_gateway_id     = aws_ec2_transit_gateway.mytgw.id
 }
