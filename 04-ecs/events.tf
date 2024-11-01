72376915c124cd49854aa8a08dab48cb44e8df08
levi-x00 <m.r.pahlevi00@gmail.com>
2024-11-01 11:26:21 +0700

    add codebuild event notification

---
+++ 04-ecs/events.tf
@@ -0,0 +1,24 @@
+#------------------------------------------------------------------------------------------------------
+# event bridge section
+#------------------------------------------------------------------------------------------------------
+resource "aws_cloudwatch_event_rule" "codebuild_status_change" {
+  name          = "codebuild-state-changes-rule"
+  description   = "Triggers on CodeBuild build status changes for success and failure."
+  role_arn      = aws_iam_role.event_publish_role.arn
+  event_pattern = <<EOF
+{
+  "source": ["aws.codebuild"],
+  "detail-type": ["CodeBuild Build State Change"],
+  "detail": {
+    "build-status": ["SUCCEEDED", "FAILED"]
+  }
+}
+EOF
+}
+
+resource "aws_cloudwatch_event_target" "send_to_sns" {
+  rule      = aws_cloudwatch_event_rule.codebuild_status_change.name
+  target_id = aws_sns_topic.topic.name
+  arn       = aws_sns_topic.topic.arn
+  # role_arn  = aws_iam_role.event_publish_role.arn
+}
