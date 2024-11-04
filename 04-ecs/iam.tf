0d8a1afec56093e100e383148d11245afd2027ba
levi-x00 <m.r.pahlevi00@gmail.com>
2024-11-04 13:03:21 +0700

    remove deprecated inline policy

--- 04-ecs/iam.tf
+++ 04-ecs/iam.tf
@@ -15,35 +15,35 @@ resource "aws_iam_role" "event_publish_role" {
   ]
 }
 EOF
-
-  inline_policy {
-    name   = "inline-policy"
-    policy = data.aws_iam_policy_document.sns_inline_policy.json
-  }
-
   tags = {
     Name = "event-publish-role"
   }
 }
 
-data "aws_iam_policy_document" "sns_inline_policy" {
-  statement {
-    sid = "PublishToSNSPolicy"
-    actions = [
-      "sns:Publish"
+resource "aws_iam_role_policy" "test_policy" {
+  name = "event-publish-policy"
+  role = aws_iam_role.event_publish_role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sns:Publish",
+        ]
+        Effect   = "Allow"
+        Resource = "${aws_sns_topic.topic.arn}"
+      },
+      {
+        Action = [
+          "kms:Encrypt*",
+          "kms:Decrypt*",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:Describe*"
+        ]
+        Effect   = "Allow"
+        Resource = "${local.kms_key_arn}"
+      }
     ]
-    resources = [aws_sns_topic.topic.arn]
-  }
-
-  statement {
-    sid = "AllowKMS"
-    actions = [
-      "kms:Encrypt*",
-      "kms:Decrypt*",
-      "kms:ReEncrypt*",
-      "kms:GenerateDataKey*",
-      "kms:Describe*"
-    ]
-    resources = [local.kms_key_arn]
-  }
+  })
 }
