d0b757c0e09718fb51501dc9cf009a2c2c068f8c
levi-x00 <m.r.pahlevi00@gmail.com>
2024-11-05 09:25:40 +0700

    update readme & deprecated inline policy

--- 04-ecs/iam.tf
+++ 04-ecs/iam.tf
@@ -15,35 +15,36 @@ resource "aws_iam_role" "event_publish_role" {
   ]
 }
 EOF
+
   tags = {
     Name = "event-publish-role"
   }
 }
 
-resource "aws_iam_role_policy" "test_policy" {
-  name = "event-publish-policy"
-  role = aws_iam_role.event_publish_role.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "sns:Publish",
-        ]
-        Effect   = "Allow"
-        Resource = "${aws_sns_topic.topic.arn}"
-      },
-      {
-        Action = [
-          "kms:Encrypt*",
-          "kms:Decrypt*",
-          "kms:ReEncrypt*",
-          "kms:GenerateDataKey*",
-          "kms:Describe*"
-        ]
-        Effect   = "Allow"
-        Resource = "${local.kms_key_arn}"
-      }
+resource "aws_iam_role_policy" "event_publish_policy" {
+  name   = "event-publish-role-policy"
+  role   = aws_iam_role.event_publish_role.id
+  policy = data.aws_iam_policy_document.sns_inline_policy.json
+}
+
+data "aws_iam_policy_document" "sns_inline_policy" {
+  statement {
+    sid = "PublishToSNSPolicy"
+    actions = [
+      "sns:Publish"
     ]
-  })
+    resources = [aws_sns_topic.topic.arn]
+  }
+
+  statement {
+    sid = "AllowKMS"
+    actions = [
+      "kms:Encrypt*",
+      "kms:Decrypt*",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:Describe*"
+    ]
+    resources = [local.kms_key_arn]
+  }
 }
