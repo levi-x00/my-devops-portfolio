adac4f9bf308c84bf7b7b3036f5191939f341b40
levi-x00 <m.r.pahlevi00@gmail.com>
2024-12-16 13:16:13 +0700

    update provider version to 5.80

--- 00-infra-backend/backend.tf
+++ 00-infra-backend/backend.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.60.0"
+      version = "~> 5.80.0"
     }
   }
-  required_version = ">=1.6.0"
+  required_version = ">=1.10.0"
 }
 
 provider "aws" {
