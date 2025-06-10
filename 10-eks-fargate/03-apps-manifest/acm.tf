815e47bbb112c8e2d5211765cf02fe1e05662fab
levi-x00 <m.r.pahlevi00@gmail.com>
2025-06-10 16:39:56 +0700

    delete unnecessary resources

--- 10-eks-fargate/03-apps-manifest/acm.tf
+++
@@ -1,9 +0,0 @@
-# Resource: ACM Certificate
-resource "aws_acm_certificate" "acm_cert" {
-  domain_name       = "*.${var.external_dns}"
-  validation_method = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
