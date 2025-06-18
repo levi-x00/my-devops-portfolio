da1bcf8e6079d6659557893600ab74d8052aade1
levi-x00 <m.r.pahlevi00@gmail.com>
2025-06-18 09:02:22 +0700

    fix network stack

--- 01-network-stack/route-tables.tf
+++ 01-network-stack/route-tables.tf
@@ -2,55 +2,65 @@
 resource "aws_default_route_table" "def-pub-rt" {
   default_route_table_id = aws_vpc.main.default_route_table_id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
   tags = {
     Name = "${var.project_name}-default-public-rt"
   }
+}
+
+resource "aws_route" "igw_default_rt" {
+  route_table_id = aws_default_route_table.def-pub-rt.id
+  gateway_id     = aws_internet_gateway.igw.id
+
+  destination_cidr_block = "0.0.0.0/0"
 }
 
 # ========================= custom private route table ==============================
 resource "aws_route_table" "private1" {
   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat1.id
-  }
-
   tags = {
     Name = "${var.project_name}-private1-rt"
   }
 }
 
+resource "aws_route" "private1_route" {
+  route_table_id = aws_route_table.private1.id
+  nat_gateway_id = aws_nat_gateway.nat1.id
+
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+
 resource "aws_route_table" "private2" {
   vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat2.id
-  }
 
   tags = {
     Name = "${var.project_name}-private2-rt"
   }
 }
 
+resource "aws_route" "private2_route" {
+  route_table_id = aws_route_table.private2.id
+  nat_gateway_id = aws_nat_gateway.nat2.id
+
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+
 # ====================== custom public route table ==============================
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
   tags = {
     Name = "${var.project_name}-public-rt"
   }
+}
+
+resource "aws_route" "public_rt_route" {
+  route_table_id = aws_route_table.public.id
+  gateway_id     = aws_internet_gateway.igw.id
+
+  destination_cidr_block = "0.0.0.0/0"
 }
 
 # ========================= associate the subnets to route table =======================
