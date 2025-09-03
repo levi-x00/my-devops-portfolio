9e2ed7e89a397e6d4c783eb1a4e76b1a7e0d3e63
levi-x00 <m.r.pahlevi00@gmail.com>
2025-09-03 07:50:32 +0700

    for now the nacl like this

--- 01-network-stack/locals.tf
+++ 01-network-stack/locals.tf
@@ -3,13 +3,106 @@ data "aws_caller_identity" "current" {}
 locals {
   account_id = data.aws_caller_identity.current.account_id
 
-  vpc_endpoints = {
-    "ec2"           = "com.amazonaws.${var.aws_region}.ec2"
-    "ssm"           = "com.amazonaws.${var.aws_region}.ssm"
-    "ssmmessages"   = "com.amazonaws.${var.aws_region}.ssmmessages"
-    "ec2messages"   = "com.amazonaws.${var.aws_region}.ec2messages"
-    "ecs-agent"     = "com.amazonaws.${var.aws_region}.ecs-agent"
-    "ecs"           = "com.amazonaws.${var.aws_region}.ecs"
-    "ecs-telemetry" = "com.amazonaws.${var.aws_region}.ecs-telemetry"
+  public_nacl_rules = {
+    ingress = [
+      {
+        rule_no     = 100
+        protocol    = "tcp"
+        action      = "allow"
+        cidr_block  = "0.0.0.0/0"
+        from_port   = 22
+        to_port     = 22
+        description = "Allow SSH"
+      },
+      {
+        rule_no     = 110
+        protocol    = "tcp"
+        action      = "allow"
+        cidr_block  = "0.0.0.0/0"
+        from_port   = 53
+        to_port     = 53
+        description = "Allow DNS resolution"
+      },
+      {
+        rule_no     = 120
+        protocol    = "udp"
+        action      = "allow"
+        cidr_block  = "0.0.0.0/0"
+        from_port   = 53
+        to_port     = 53
+        description = "Allow DNS resolution"
+      },
+      {
+        rule_no     = 130
+        protocol    = "tcp"
+        action      = "allow"
+        cidr_block  = "0.0.0.0/0"
+        from_port   = 80
+        to_port     = 80
+        description = "Allow HTTP"
+      },
+      {
+        rule_no     = 140
+        protocol    = "tcp"
+        action      = "allow"
+        cidr_block  = "0.0.0.0/0"
+        from_port   = 443
+        to_port     = 443
+        description = "Allow HTTPS"
+      },
+      {
+        rule_no     = 150
+        protocol    = "tcp"
+        action      = "allow"
+        cidr_block  = "0.0.0.0/0"
+        from_port   = 1024
+        to_port     = 65535
+        description = "Allow ephemeral ports"
+      },
+      {
+        rule_no     = 160
+        protocol    = "udp"
+        action      = "allow"
+        cidr_block  = "0.0.0.0/0"
+        from_port   = 1024
+        to_port     = 65535
+        description = "Allow ephemeral ports"
+      },
+      {
+        rule_no     = 170
+        protocol    = "-1"
+        action      = "allow"
+        cidr_block  = "0.0.0.0/0"
+        from_port   = 0
+        to_port     = 0
+        description = "Allow icmp"
+      }
+    ]
+  }
+
+  private_nacl_rules = {
+    ingress = [
+      {
+        rule_no     = 100
+        protocol    = "tcp"
+        action      = "allow"
+        cidr_block  = "10.0.0.0/23"
+        from_port   = 0
+        to_port     = 65535
+        description = "Allow inbound from public subnet"
+      }
+    ]
+
+    egress = [
+      {
+        rule_no     = 100
+        protocol    = "tcp"
+        action      = "allow"
+        cidr_block  = "0.0.0.0/0"
+        from_port   = 0
+        to_port     = 65535
+        description = "Allow outbound to public subnet"
+      }
+    ]
   }
 }
--- 01-network-stack/nacl.tf
+++ 01-network-stack/nacl.tf
@@ -1,4 +1,4 @@
-resource "aws_default_network_acl" "def-nacl" {
+resource "aws_default_network_acl" "public_nacl" {
   default_network_acl_id = aws_vpc.main.default_network_acl_id
 
   subnet_ids = [
@@ -10,55 +10,103 @@ resource "aws_default_network_acl" "def-nacl" {
     aws_subnet.db-1b.id
   ]
 
-  dynamic "ingress" {
-    for_each = [
-      { rule_no = 90, protocol = "6", action = "allow", from = 22, to = 22 },         # SSH
-      { rule_no = 95, protocol = "6", action = "allow", from = 53, to = 53 },         # DNS TCP
-      { rule_no = 96, protocol = "17", action = "allow", from = 53, to = 53 },        # DNS UDP
-      { rule_no = 100, protocol = "6", action = "allow", from = 80, to = 80 },        # HTTP
-      { rule_no = 110, protocol = "6", action = "allow", from = 443, to = 443 },      # HTTPS
-      { rule_no = 120, protocol = "6", action = "allow", from = 8200, to = 8200 },    # Vault
-      { rule_no = 130, protocol = "6", action = "allow", from = 9200, to = 9200 },    # SonarQube
-      { rule_no = 140, protocol = "17", action = "allow", from = 13231, to = 13231 }, # WireGuard
-      { rule_no = 150, protocol = "6", action = "allow", from = 1024, to = 65535 },   # Ephemeral TCP
-      { rule_no = 160, protocol = "17", action = "allow", from = 1024, to = 65535 },  # Ephemeral UDP
-      { rule_no = 170, protocol = "-1", action = "allow", from = 0, to = 0 },         # ICMP
-    ]
-    content {
-      rule_no    = ingress.value.rule_no
-      protocol   = ingress.value.protocol
-      action     = ingress.value.action
-      cidr_block = "0.0.0.0/0"
-      from_port  = ingress.value.from
-      to_port    = ingress.value.to
-    }
+  ingress {
+    rule_no    = "100"
+    protocol   = "-1"
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
   }
 
-  dynamic "egress" {
-    for_each = [
-      { rule_no = 90, protocol = "6", action = "allow", from = 22, to = 22 },         # SSH
-      { rule_no = 95, protocol = "6", action = "allow", from = 53, to = 53 },         # DNS TCP
-      { rule_no = 96, protocol = "17", action = "allow", from = 53, to = 53 },        # DNS UDP
-      { rule_no = 100, protocol = "6", action = "allow", from = 80, to = 80 },        # HTTP
-      { rule_no = 110, protocol = "6", action = "allow", from = 443, to = 443 },      # HTTPS
-      { rule_no = 120, protocol = "6", action = "allow", from = 8200, to = 8200 },    # Vault
-      { rule_no = 130, protocol = "6", action = "allow", from = 9200, to = 9200 },    # SonarQube
-      { rule_no = 140, protocol = "17", action = "allow", from = 13231, to = 13231 }, # WireGuard
-      { rule_no = 150, protocol = "6", action = "allow", from = 1024, to = 65535 },   # Ephemeral TCP
-      { rule_no = 160, protocol = "17", action = "allow", from = 1024, to = 65535 },  # Ephemeral UDP
-      { rule_no = 170, protocol = "-1", action = "allow", from = 0, to = 0 },         # ICMP
-    ]
-    content {
-      rule_no    = egress.value.rule_no
-      protocol   = egress.value.protocol
-      action     = egress.value.action
-      cidr_block = "0.0.0.0/0"
-      from_port  = egress.value.from
-      to_port    = egress.value.to
-    }
+  egress {
+    rule_no    = "100"
+    protocol   = "-1"
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
   }
 
   tags = {
     Name = "${var.project_name}-default-nacl"
   }
 }
+
+resource "aws_network_acl" "public_nacl" {
+  vpc_id = aws_vpc.main.id
+
+  subnet_ids = [
+    # aws_subnet.public-1a.id,
+    # aws_subnet.public-1b.id,
+    # aws_subnet.private-1a.id,
+    # aws_subnet.private-1b.id,
+    # aws_subnet.db-1a.id,
+    # aws_subnet.db-1b.id
+  ]
+
+  dynamic "ingress" {
+    for_each = local.public_nacl_rules.ingress
+    content {
+      rule_no    = ingress.value.rule_no
+      protocol   = ingress.value.protocol
+      action     = ingress.value.action
+      cidr_block = ingress.value.cidr_block
+      from_port  = ingress.value.from_port
+      to_port    = ingress.value.to_port
+    }
+  }
+
+  dynamic "egress" {
+    for_each = local.public_nacl_rules.ingress
+    content {
+      rule_no    = egress.value.rule_no
+      protocol   = egress.value.protocol
+      action     = egress.value.action
+      cidr_block = "0.0.0.0/0"
+      from_port  = egress.value.from_port
+      to_port    = egress.value.to_port
+    }
+  }
+
+  tags = {
+    Name = "${var.project_name}-public-nacl"
+  }
+}
+
+resource "aws_network_acl" "private_nacl" {
+  vpc_id = aws_vpc.main.id
+
+  subnet_ids = []
+
+  dynamic "ingress" {
+    for_each = local.private_nacl_rules.ingress
+    content {
+      rule_no    = ingress.value.rule_no
+      protocol   = ingress.value.protocol
+      action     = ingress.value.action
+      cidr_block = ingress.value.cidr_block
+      from_port  = ingress.value.from_port
+      to_port    = ingress.value.to_port
+    }
+  }
+
+  dynamic "egress" {
+    for_each = local.private_nacl_rules.egress
+    content {
+      rule_no    = egress.value.rule_no
+      protocol   = egress.value.protocol
+      action     = egress.value.action
+      cidr_block = egress.value.cidr_block
+      from_port  = egress.value.from_port
+      to_port    = egress.value.to_port
+    }
+  }
+
+  tags = {
+    Name = "${var.project_name}-private-nacl"
+  }
+}
+
+
+
