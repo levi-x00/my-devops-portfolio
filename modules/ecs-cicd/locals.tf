c0cc62da158b472cd1405606052dd3325eaf1327
levi-x00 <m.r.pahlevi00@gmail.com>
2025-09-10 20:50:58 +0700

    remove tfvars

--- modules/ecs-cicd/locals.tf
+++ modules/ecs-cicd/locals.tf
@@ -13,5 +13,5 @@ locals {
   sns_arn         = local.ecs.sns_arn
 
   account_id = data.aws_caller_identity.current.account_id
-  region     = data.aws_region.current.name
+  region     = data.aws_region.current.region
 }
