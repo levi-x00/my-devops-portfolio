10190baca103c1c0eb96fa09bfcea3ce94ede3f9
levi-x00 <m.r.pahlevi00@gmail.com>
2025-10-17 16:18:57 +0700

    update eks fargate local

--- 10-eks-fargate/01-eks-manifest/locals.tf
+++ 10-eks-fargate/01-eks-manifest/locals.tf
@@ -4,25 +4,13 @@ locals {
   network_info = data.terraform_remote_state.network.outputs
   kms_key_arn  = local.network_info.kms_key_arn
   kms_key_id   = local.network_info.kms_key_id
-  vpc_id = local.network_info.vpc_id
+  vpc_id       = local.network_info.vpc_id
 
   private_subnet_ids = local.network_info.private_subnet_ids
 
-  kms_key_id = data.terraform_remote_state.network.outputs.kms_key_id
 
-  lb_subnets     = data.terraform_remote_state.network.outputs.public_subnet_ids
-  prv_subnets    = data.terraform_remote_state.network.outputs.private_subnet_ids
   vpc_cidr_block = data.terraform_remote_state.network.outputs.vpc_cidr_block
   account_id     = data.aws_caller_identity.current.account_id
 
   aws_iam_oidc_connect_provider_extract_from_arn = element(split("oidc-provider/", "${aws_iam_openid_connect_provider.eks.arn}"), 1)
-}
-
-locals {
-  network_info = data.terraform_remote_state.network.outputs
-  kms_key_arn  = local.network_info.kms_key_arn
-  # kms_key_id   = local.network_info.kms_key_id
-  vpc_id = local.network_info.vpc_id
-
-  private_subnet_ids = local.network_info.private_subnet_ids
 }
