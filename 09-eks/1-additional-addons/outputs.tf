cb1f7b41f929fd9208cb115b27f76d3337673454
levi-x00 <m.r.pahlevi00@gmail.com>
2026-02-11 20:34:24 +0700

    remove additional add-ons

--- 09-eks/1-additional-addons/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "lbc_helm_metadata" {
-  value = helm_release.load_balancer_controller.metadata
-}
-
-output "autoscaler_helm_metadata" {
-  value = helm_release.cluster_autoscaler_release.metadata
-}
